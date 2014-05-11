bd0616315ba690fc867dee29a2cef7501b5f98c0
Qian Xu <sx.away@googlemail.com>
2014-05-11 23:48:40 +0800

    changes to l10n

--- vcl/src/dui.text.L10n.pas
+++ vcl/src/dui.text.L10n.pas
@@ -1,5 +1,5 @@
 (**
- * $Id: dui.text.L10n.pas 506 2012-05-15 16:30:06Z QXu $
+ * $Id: dui.text.L10n.pas 820 2014-05-11 15:47:32Z QXu $
  *
  * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
  * express or implied. See the License for the specific language governing rights and limitations under the License.
@@ -15,10 +15,14 @@ type
   public
     constructor Create;
     destructor Destroy; override;
-    function Require(Identifier: Cardinal; const DefaultText: string): string;
+    function Get(Identifier: Cardinal; const DefaultText: string): string; overload;
+    function Get(Identifier: Cardinal; const DefaultText: string; const Args: array of const): string; overload;
   end;
 
 implementation
+
+uses
+  System.SysUtils;
 
 constructor TL10n.Create;
 begin
@@ -30,10 +34,16 @@ begin
   inherited;
 end;
 
-function TL10n.Require(Identifier: Cardinal; const DefaultText: string): string;
+function TL10n.Get(Identifier: Cardinal; const DefaultText: string): string;
 begin
   // TODO:
   Result := DefaultText;
+end;
+
+function TL10n.Get(Identifier: Cardinal; const DefaultText: string; const Args: array of const): string;
+begin
+  // TODO:
+  Result := Format(DefaultText, Args);
 end;
 
 end.
